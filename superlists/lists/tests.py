e6dcc07d2ed0f12c4965a30927e1295b09423f35
susocket <susocket@sina.com>
2016-06-15 00:24:50 +0800

    20160615.1

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,8 +1,24 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from lists.views import home_page
+from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 # Create your tests here.
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-  def test_bad_maths(self):
-    self.assertEqual(1+1, 2)
+  def test_root_url_resolves_to_home_page_view(self):
+    found = resolve('/')
+    self.assertEqual(found.func, home_page)
+
+  def test_home_page_returns_correct_html(self):
+    request = HttpRequest()
+    response = home_page(request)
+    self.assertTrue(response.content.startswith(b'<html>'))
+    self.assertIn(b'<title>To-Do lists</title>', response.content)
+    print(repr(response.content))
+    self.assertTrue(response.content.strip().endswith(b'</html>'))
+
+    expect_html = render_to_string('home.html')
+    self.assertEqual(response.content.decode(), expect_html)
